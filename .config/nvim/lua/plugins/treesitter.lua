6d6027b44cedd45a6bc1afea0629a820d42c6ba5
Ismail Zilic <ismailzilic@gmail.com>
2026-05-05 15:16:00 +0200

    nvim new treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,10 @@
 vim.pack.add({ {
-	src = "https://github.com/nvim-treesitter/nvim-treesitter",
-	version = "main",
-	data = {
-		run = function()
-			vim.cmd(":TSUpdate")
-		end
-	}
+	src = "https://github.com/romus204/tree-sitter-manager.nvim",
 } })
 
-require("nvim-treesitter").setup({
-	ensure_installed = { "vim", "vimdoc", "markdown", "asm", "c", "cpp", "make", "cmake", "lua", "typescript", "javascript", "bash" },
+require("tree-sitter-manager").setup({
+	ensure_installed = { "vim", "vimdoc", "markdown", "asm", "c", "cpp", "rust", "python", "make", "cmake", "lua", "typescript", "javascript", "bash" },
 	auto_install = true,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	indent = { enable = true },
+	highlight = true,
+	border = nil,
 })
